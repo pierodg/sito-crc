b74c7a964da176f25c530ce1d79dbc7d5251b835
passasooz <francescopassanante@gmail.com>
2020-07-14 11:00:26 +0200

    privacy layout

---
+++ privacy.php
@@ -0,0 +1,124 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>CRC - Applicazioni</title>
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/swiper-bundle.min.css">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="css/style.css">
+
+    </head>
+    <body>
+        <svg display="none" xmlns="http://www.w3.org/2000/svg">
+        <symbol id="logo" viewBox="0 0 218 47" id=".3287818552427719" xmlns="http://www.w3.org/2000/svg">
+        <path fill="black" class="ast3" d="M35.02.78h20.69c.58 0 .95.21 1.13.64-1.81 5.56-2.83 8.48-3.04 8.76l-.79.15h-24.2c-6.22 0-9.82.43-10.82 1.28-1.8 1.54-3.55 5.42-5.25 11.63-.11 1.33-.17 2.07-.17 2.23 0 1.48 1.01 2.21 3.04 2.21 0 .26 2.54.43 7.63.49h24.2c.15 0 .31.16.47.47-1.69 5.68-2.75 8.7-3.17 9.08l-.81.15H18.78c-9.68 0-15.41-.95-17.18-2.85C.53 33.62 0 32.08 0 30.39 0 26.58 2.01 19.78 6.04 10c1.95-3.93 4.87-6.58 8.76-7.95C16.6 1.2 23.34.78 35.02.78zM2.55 29.12c.05 1.28.21 1.91.47 1.91V29.9c.06-3.21 2.13-9.53 6.21-18.95 2.53-4.66 7.14-7 13.84-7 .19-.11 1.78-.17 4.78-.17h24.03l1.13-.15v-.47H28.17c-9.08 0-14.44.37-16.08 1.13-1.84.9-3.16 2.65-3.98 5.25-3.7 9.16-5.56 15.69-5.56 19.58zm9.7.64v.15c1.6.75 4.95 1.13 10.04 1.13H43.3l.81-.17v-.47H18.95c-2.01 0-4.19-.21-6.53-.64h-.17zM53.8 26.08h10.5c.16 0 .33.21.49.64-2.2 6.84-3.43 10.29-3.68 10.35-.34.21-.65.32-.94.32h-10.2c-.15 0-.31-.16-.47-.47 2.05-7.12 3.27-10.67 3.66-10.67.19-.11.41-.17.64-.17zm-.31 1.12v.15c0 .58.16 1.16.49 1.76-1.06 3.13-1.59 4.93-1.59 5.4h.32c.65 0 1.08-.53 1.28-1.59.73-2.65 1.25-3.98 1.58-3.98h5.1c.21 0 .32-.11.32-.32v-.15l-.79-.17h-4.8c-.76-.73-1.4-1.1-1.91-1.1zM109.04.94c6.25.46 9.38 1.36 9.38 2.7.51.28.83 1.39.96 3.34 0 2.79-1.27 6.82-3.81 12.1-1.3 2.15-3.21 3.58-5.74 4.3v.17c2.01.83 3.02 2.36 3.02 4.61 1.39 7.39 2.33 13.31 2.83 17.74l-.47.32H103.9c-.55 0-1.33-3.05-2.34-9.15-.78-5.1-1.26-7.75-1.44-7.95-.95-.33-1.58-.49-1.89-.49H79.59c-.79 0-1.32.8-1.59 2.4-1.38 4.39-2.23 6.61-2.55 6.68l-.79.15h-9.57c-.15 0-.31-.16-.47-.47v-.15c0-.28 3.82-12.16 11.46-35.66l.64-.49h28.49c1.05 0 2.33-.05 3.83-.15zM76.87 2.22v.15c0 .61.16 1.14.49 1.59 0 .24-3.35 10.53-10.04 30.88l.32.17h.17c.85 0 1.28-.69 1.28-2.08 1.26-3.65 4.45-13.31 9.55-28.96h30.87c1.91 0 3.61.21 5.1.64v-.33c-1.95-.64-4.4-.96-7.33-.96H79.27c-.36 0-1-.37-1.91-1.11h-.49zm32 8.42v1.13c0 1.43-.74 3.23-2.23 5.4-1.71 1-2.61 1.69-2.7 2.08-.84 1.48-3.06 2.21-6.68 2.21H81.97c-.24 0-.45-.05-.64-.15-.2.1-.36.15-.47.15v.32c1.14.22 2.36.32 3.66.32h9.38c8.58 0 12.88-.69 12.91-2.06.65-.27 1.39-1.98 2.23-5.1l.32-2.38c-.09-1.28-.25-1.92-.49-1.92zm-26.9 8.29l.47.32h8.44c9.03 0 13.54-.48 13.54-1.44.7-.64 1.39-2.33 2.06-5.08v-.64c0-1.06-1.33-1.59-3.98-1.59H85.63c-.98 0-1.72 1.33-2.23 3.98-.43.91-.9 2.4-1.43 4.45z" fill="#0e0e0e"/>
+        <path fill="black" class="ast3" d="M119.92 26.08h10.5c.16 0 .33.21.49.64-2.2 6.84-3.43 10.29-3.68 10.35-.34.21-.65.32-.94.32h-10.2c-.15 0-.31-.16-.47-.47 2.05-7.12 3.27-10.67 3.66-10.67.19-.11.4-.17.64-.17zm-.32 1.12v.15c0 .58.16 1.16.49 1.76-1.06 3.13-1.59 4.93-1.59 5.4h.32c.65 0 1.08-.53 1.28-1.59.73-2.65 1.25-3.98 1.58-3.98h5.1c.21 0 .32-.11.32-.32v-.15l-.79-.17h-4.78c-.78-.73-1.42-1.1-1.93-1.1zM184.65.78h20.69c.58 0 .95.21 1.13.64-1.81 5.56-2.83 8.48-3.04 8.76l-.79.15h-24.2c-6.22 0-9.82.43-10.82 1.28-1.8 1.54-3.55 5.42-5.25 11.63-.11 1.33-.17 2.07-.17 2.23 0 1.48 1.01 2.21 3.04 2.21 0 .26 2.54.43 7.63.49h24.2c.15 0 .31.16.47.47-1.69 5.68-2.75 8.7-3.17 9.08l-.81.15h-25.15c-9.68 0-15.41-.95-17.18-2.85-1.06-1.39-1.59-2.93-1.59-4.61 0-3.81 2.01-10.61 6.04-20.39 1.95-3.93 4.87-6.58 8.76-7.95 1.79-.87 8.53-1.29 20.21-1.29zm-32.47 28.34c.05 1.28.21 1.91.47 1.91V29.9c.06-3.21 2.13-9.53 6.21-18.95 2.53-4.66 7.14-7 13.84-7 .19-.11 1.78-.17 4.78-.17h24.03l1.13-.15v-.47H177.8c-9.08 0-14.44.37-16.08 1.13-1.84.9-3.16 2.65-3.98 5.25-3.7 9.16-5.56 15.69-5.56 19.58zm9.7.64v.15c1.6.75 4.95 1.13 10.04 1.13h21.01l.81-.17v-.47h-25.15c-2.01 0-4.19-.21-6.53-.64h-.18zM207.01 26.08h10.5c.16 0 .33.21.49.64-2.2 6.84-3.43 10.29-3.68 10.35-.34.21-.65.32-.94.32h-10.2c-.15 0-.31-.16-.47-.47 2.05-7.12 3.27-10.67 3.66-10.67.19-.11.4-.17.64-.17zm-.32 1.12v.15c0 .58.16 1.16.49 1.76-1.06 3.13-1.59 4.93-1.59 5.4h.32c.65 0 1.08-.53 1.28-1.59.73-2.65 1.25-3.98 1.58-3.98h5.1c.21 0 .32-.11.32-.32v-.15l-.79-.17h-4.78c-.78-.73-1.42-1.1-1.93-1.1z" fill="#0e0e0e"/>
+        <g>
+        <path fill="black" class="ast3" d="M148.93 26.18l-4.34 5.74 2.7 3.65-2.91 2.17-2.11-2.71c-.79.85-1.58 1.52-2.39 2-.95.54-1.91.82-2.89.82-1.51 0-2.77-.46-3.79-1.37-1.02-.91-1.53-2.09-1.53-3.53 0-.39.04-.75.11-1.08.22-1.25.75-2.35 1.59-3.29.74-.83 1.73-1.56 2.97-2.2-.34-.44-.6-.88-.77-1.3-.17-.43-.26-.89-.26-1.39 0-.25.02-.51.08-.77.26-1.41.93-2.57 2.02-3.51 1.09-.93 2.33-1.4 3.74-1.4 1.2 0 2.18.36 2.94 1.07.76.71 1.15 1.63 1.15 2.76 0 .28-.02.56-.08.85-.21 1.08-.64 2-1.29 2.77s-1.52 1.41-2.6 1.92l1.39 1.96 2.47-3.16h3.8zm-8.56 6.25l-2.35-3.31c-.75.28-1.35.64-1.79 1.05-.57.51-.92 1.13-1.05 1.84-.03.24-.05.41-.05.51 0 .6.21 1.08.62 1.44.41.36.93.54 1.57.54.6 0 1.18-.22 1.74-.65.42-.3.86-.78 1.31-1.42zm1.51-9.56c.03-.07.04-.18.04-.34 0-.8-.46-1.2-1.39-1.2-.44 0-.84.14-1.18.43-.34.28-.56.65-.64 1.09-.02.07-.04.17-.04.3 0 .38.15.79.44 1.24.11.15.26.36.46.64.65-.28 1.13-.54 1.44-.8.48-.38.77-.83.87-1.36z" fill="#0e0e0e"/>
+        </g>
+        <path fill="black" class="ast3" d="M137.48 36.87s-6.9-1.05-3.35-7.09c0 0-5.17 6.42 3.35 7.09z" fill="#0e0e0e"/>
+        </symbol>
+        </svg>
+        <div class="container w-2220">
+            <nav class="navmenu navbar navbar-expand-sm border-bottom border-dark">
+                <a class="navbar-brand" href="#"><svg class="logo"><use xlink:href="#logo" xmlns:xlink="http://www.w3.org/1999/xlink"></use></svg></a>
+                <ul class="navbar-nav ml-auto">
+                    <li class="mr-4 nav-item">
+                        <a class="nav-link" href="#">Servizi</a>
+                    </li>
+                    <li class="mr-4 nav-item">
+                        <a class="nav-link" href="#">Applicazioni</a>
+                    </li>
+                    <li class="mr-4 nav-item">
+                        <a class="nav-link" href="#">Chi siamo</a>
+                    </li>
+                    <li class="mr-4 nav-item">
+                        <a class="nav-link" href="#">IT</a>
+                    </li>
+                    <li class="mr-4 nav-item">
+                        <a class="nav-link" href="#">Menu</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        
+        <div class="container w-2220 p-4">
+            <div class="row py-4">
+                <div class="col-8 offset-1">
+                    <h1 class="font-regular uppercase">Privacy Policy</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-9 offset-1">
+                    <h2 class="font-regular">Subtitle lorem ipsum</h2>
+                    <p class="font-light py-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quisquam modi laboriosam, fugit numquam obcaecati. Eveniet itaque sapiente, harum in veritatis vero, temporibus et reiciendis fugit blanditiis doloribus vel excepturi.</p>
+                    <ul class="custom-list">
+                        <li>lorem ipsum dolor sit amet</li>
+                        <li>lorem ipsum dolor sit amet</li>
+                        <li>lorem ipsum dolor sit amet</li>
+                        <li>lorem ipsum dolor sit amet</li>
+                        <li>lorem ipsum dolor sit amet</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        
+       
+        <footer class="container w-2220 font-light">
+            <div class="row line-above pt-5 pb-3">
+                <div class="col-6 pl-0">
+                    <h2 class="font-regular">CR&C srl</h2>
+                    <p class="mt-4">Via Thomas Edison 2<br>20875 - Burago di Molgora(MB)<br>Italy</p>
+                    <p>P.Iva: IT04421030968</p>
+                </div>
+    
+                <div class="col-2">
+                    <ul class="navbar-nav">
+                        <li><a href="#">Servizi</a></li>
+                        <li><a href="#">Applicazioni</a></li>
+                        <li><a href="#">Chi siamo</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Contatti</a></li>
+                    </ul>
+                </div>
+                <div class="col-2">
+                    <ul class="navbar-nav">
+                        <li><i class="icon-phone"></i>+39 039 6200005</li>
+                        <li><i class="icon-email"></i>info@crcsrl.biz</li>
+                    </ul>
+                </div>
+                <div class="col-2 d-flex">
+                    <h3 class="text-rotate font-regular">Competence Research &amp; Consulting</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 pl-0">
+                    <ul class="social-bar">
+                        <li>facebook</li>
+                        <li>instagram</li>
+                        <li>linkedin</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row py-3">
+                <div class="col-12 d-flex align-items-center justify-content-between px-0">
+                    <span class="font-regular">&copy; CR&amp;C Srl - Privacy Policy</span>
+                    <span>Bespoken</span>
+                </div>
+            </div>
+        </footer>
+
+
+
+        <script src="js/jquery-3.5.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/swiper-bundle.min.js"></script>
+    </body>
+</html>
