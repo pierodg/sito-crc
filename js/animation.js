46ff2544af71106bbd9013859c0d4d587903c9ed
Piero <dg_piero@hotmail.it>
2020-07-22 11:51:14 +0200

    test animation

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,18 @@
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.to(".fade-2, .fade-1",
+{
+    scrollTrigger: {
+        trigger: ".fade-2, .fade-1",
+        markers: true,
+        start: "100px",
+        toggleActions:"play none restart none"
+    },
+        duration: 2, 
+        x: 300,
+        ease: Sine.easeOut, 
+        opacity: 1 });
+/*
 gsap.to(".fade-1", {
     scrollTrigger: {
         trigger: ".fade-1",
@@ -20,37 +33,5 @@ gsap.to(".fade-2", {
     },
     opacity: 1,
     duration: 3
-});
+});*/
 
-gsap.to(".fade-3", {
-    scrollTrigger: {
-        trigger: ".fade-3",
-        markers: true,
-        start: "0%",
-        toggleActions: "play none none none"
-    },
-    opacity: 1,
-    duration: 3
-});
-
-gsap.to(".fade-4", {
-    scrollTrigger: {
-        trigger: ".fade-4",
-        markers: true,
-        start: "0%",
-        toggleActions: "play none none none"
-    },
-    opacity: 1,
-    duration: 3
-});
-
-gsap.to(".fade-5", {
-    scrollTrigger: {
-        trigger: ".fade-5",
-        markers: true,
-        start: "0%",
-        toggleActions: "play none none none"
-    },
-    opacity: 1,
-    duration: 3
-});
